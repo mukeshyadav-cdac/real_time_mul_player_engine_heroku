59cebb106cfb592bc78dbc19438c3c0299485821
Mukesh Yadav <ashu.3333d@gmail.com>
2013-08-19 01:09:10 +0530

    added hostname

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,33 +1,5 @@
-// var frame_time = 1000/60; // run the local game at 16ms/ 60hz
-// //if('undefined' != typeof(global)) frame_time = 45; //on server we run at 45ms, 22hz
-// ( function () {
-// 		var lastTime = 0;
-//     var vendors = [ 'ms', 'moz', 'webkit', 'o' ];
-
-//     // for ( var x = 0; x < vendors.length && !window.requestAnimationFrame; ++ x ) {
-//     //     window.requestAnimationFrame = window[ vendors[ x ] + 'RequestAnimationFrame' ];
-//     //     window.cancelAnimationFrame = window[ vendors[ x ] + 'CancelAnimationFrame' ] || window[ vendors[ x ] + 'CancelRequestAnimationFrame' ];
-//     // }
-
-//     //if ( !window.requestAnimationFrame ) {
-//         window.requestAnimationFrame = function ( callback, element ) {
-//             var currTime = Date.now(), timeToCall = Math.max( 0, frame_time - ( currTime - lastTime ) );
-//             var id = window.setTimeout( function() { callback( currTime + timeToCall ); }, timeToCall );
-//             lastTime = currTime + timeToCall;
-//             return id;
-//         };
-//     //}
-
-//     if ( !window.cancelAnimationFrame ) {
-//         window.cancelAnimationFrame = function ( id ) { clearTimeout( id ); };
-//     }
-
-// }() );
-
-// var circle = [];
-// var color = ['blank','red','black','green','blue', 'white'];
 function start() {
-	var socket = io.connect('http://localhost');
+	var socket = io.connect(window.location.origin);
 	canvasE = document.getElementById('canvas');
   con = canvasE.getContext('2d');
   count = 1;
@@ -37,12 +9,6 @@ function start() {
 	
 	socket.on('start', function (data) {
   	game.initialize(data);
-  	// for (var i = 1 ; i <= 5; i++) {
- 		// 	circle[i] = new Circle(new Point( data[i][0], data[i][1]), data[i][2]);
- 		// };
- 		// if( count == 1)
- 		// 	renderme();
- 		// count = count + 1;		 
 	});
 
 	socket.on('checkSync', function (data) {
@@ -55,26 +21,4 @@ function start() {
 
  		}
 	});
-
-	
 }
-
-// function renderme() {
-
-// 	con.clearRect( 0, 0, 900, 600 );
-	
-// 	for (var i = 1; i <= 5; i++) {
-// 		var  circleFirst = circle[i];
-// 		for (var j = i+1; j <= 5; j++ ) {
-// 			Physics.collisionDetection( circleFirst, circle[j] );	
-// 			Physics.collisionResolve( circleFirst, circle[j] );
-// 		};
-// 	};
-	
-// 	for (var i = 1; i <= 5; i++) {
-// 		circle[i].update();
-// 		circle[i].draw( con, color[i] );
-// 	};
-// 	window.requestAnimationFrame( renderme );	
-// }
-
